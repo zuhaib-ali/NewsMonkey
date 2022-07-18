36b2d857d5c3b9fcff197941971ae2546786e3f6
zuhaib-ali <zuhaibsoomro25@gmail.com>
2022-07-18 15:25:02 +0500

    updated news monkey

--- src/Components/News.js
+++ src/Components/News.js
@@ -19,7 +19,7 @@ export default class News extends Component {
       return str.charAt(0).toUpperCase() + str.slice(1);
     }
 
-    async updateNews(){
+    updateNews = async () => {
       let data = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=e2ce6e6733a24cd989b53a091c968217&page=${this.state.page+1}&pageSize=${this.state.pageSize}&category=${this.props.category}`);
       let articles = await data.json();
       this.setState({  
@@ -31,38 +31,40 @@ export default class News extends Component {
       });  
     }
 
+    moveToTop = () => {
+      window.scrollTo(0,0);
+    }
+
     async componentDidMount(){
       document.title = `${this.capitalize(this.props.category)}`;
       this.updateNews();
     }
 
-    // Next Button
-    nextButtonHandler = async () => {  
-      this.updateNews(); 
-    }
-
   render() {
     return (
-      <div>
+      <div id="main">
         {/* Loading */}
         {
           this.state.loading ? 
-          <div className="spinner-border" role="status" style={{zIndex:"100", position:"absolute", left:"50%", top:"50%"}}><span class="sr-only"></span></div> : 
+          <div className="spinner-border" role="status" style={{zIndex:"100", position:"absolute", left:"50%", top:"50%"}}><span className="sr-only"></span></div> : 
           ""
         }
 
         <h2 className='text-center m-5'>Welcome - {this.props.category} Top Headlines  </h2>
 
-        {/* NEWS ITEMS */}
-        <div style={{display:"flex", flexWrap:"wrap"}}>
-          {this.state.article.map((element) => {
-            return <NewsItem source={element.source.name} title={element.title.slice(0, 80)} description={element.description !== null? element.description.slice(0,100): element.description} image={element.urlToImage} url={element.url} author={element.author ? element.author : "unknown"} publishedAt={new Date(element.publishedAt).toLocaleString()}></NewsItem>      
+        {/* NEWS ITEMS */} 
+        <div style={{display:"flex", flexWrap:"wrap", justifyContent:"space-between"}}>
+          {this.state.article.map((element, index) => {
+            return <NewsItem key={index} source={element.source.name} title={element.title.slice(0, 80)} description={element.description !== null? element.description.slice(0,100): element.description} image={element.urlToImage} url={element.url} author={element.author ? element.author : "unknown"} publishedAt={new Date(element.publishedAt).toLocaleString()}></NewsItem>
           })}  
         </div>
 
-        {/* BUTTONS */}
         <div style={{display:"flex", justifyContent:"center", margin:"50px 0px"}} hidden={this.state.article.length === 0}>
-          <button className="btn btn-outline-light" style={{color:"black"}} onClick={this.nextButtonHandler} hidden={this.state.page === this.state.totalPages}>Load More &darr;</button>
+          <button className="btn btn-outline-dark" onClick={this.updateNews} hidden={this.state.page === this.state.totalPages}>Load More &darr;</button>
+        </div>
+
+        <div style={{display:"flex", justifyContent:"flex-end", textShadow:"0px 0px 5px lightgrey"}}>
+          <button className="btn btn-light" onClick={this.moveToTop}>&Lambda;</button>
         </div>
       </div>
     )
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -3,23 +3,23 @@ import React, { Component } from 'react'
 export default class NewsItem extends Component {
   render() {
     return (
-        <div class="card" style={{width: "20rem", margin:"10px"}}>
-            <span class="position-absolute top-0 start-50 translate-middle badge rounded-pill bg-warning" style={{zIndex:"50"}}>{this.props.source}</span>
+        <div className="card" style={{width:"320px", margin:"20px 0px"}}>
+            <span className="position-absolute top-0 start-50 translate-middle badge rounded-pill bg-warning" style={{zIndex:"50"}}>{this.props.source}</span>
             
             {/* Image */}
-            <img src={this.props.image} class="card-img-top" alt="..."/>
+            <img src={this.props.image} className="card-img-top" alt="..."/>
             
-            <div class="card-body">
-                <h5 class="card-title">{this.props.title}</h5>
-                <p class="card-text">{this.props.description}</p>
+            <div className="card-body">
+                <h5 className="card-title">{this.props.title}</h5>
+                <p className="card-text">{this.props.description}</p>
             </div>
 
             {/* Author and Published Date */}
-            <p class="card-text d-flex justify-content-between mx-2">
-              <small class="text-muted">{this.props.author}</small>
-              <small class="text-muted">{this.props.publishedAt}</small>
+            <p className="card-text d-flex justify-content-between mx-2">
+              <small className="text-muted">{this.props.author}</small>
+              <small className="text-muted">{this.props.publishedAt}</small>
             </p>
-            <a href={this.props.url} class="btn btn-dark btn-sm m-2">View</a>
+            <a href={this.props.url} className="btn btn-dark btn-sm m-2">View</a>
         </div>
     )
   }
